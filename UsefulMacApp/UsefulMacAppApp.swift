f5b5f29e48a5c366455832f0fce57ee048c438e6
Aarav Kapur <aarav.k.kapur@gmail.com>
2025-08-20 15:12:14 -0400

    Initial Commit

---
+++ UsefulMacApp/UsefulMacAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsefulMacAppApp.swift
+//  UsefulMacApp
+//
+//  Created by Aarav Kapur on 8/20/25.
+//
+
+import SwiftUI
+
+@main
+struct UsefulMacAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UsefulMacAppTests/UsefulMacAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  UsefulMacAppTests.swift
+//  UsefulMacAppTests
+//
+//  Created by Aarav Kapur on 8/20/25.
+//
+
+import Testing
+@testable import UsefulMacApp
+
+struct UsefulMacAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
